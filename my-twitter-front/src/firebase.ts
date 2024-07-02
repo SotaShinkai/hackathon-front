9b6d4de6caff0bf6a55548d1781e125fea6510bc
Tenho-basic <tenhoubuntu@gmail.com>
2024-07-02 13:13:36 +0900

    firebase

--- my-twitter-front/src/firebase.ts
+++ my-twitter-front/src/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCN0VXyJh3PUfhuJ0bSFE8itC3I1H4ReSs",
+  authDomain: "my-twitter-fb8ef.firebaseapp.com",
+  projectId: "my-twitter-fb8ef",
+  storageBucket: "my-twitter-fb8ef.appspot.com",
+  messagingSenderId: "289390394952",
+  appId: "1:289390394952:web:fc39be524c413b2eb5227d",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
